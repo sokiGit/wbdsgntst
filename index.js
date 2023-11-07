1fc43d334852d318f94c0f386dacde06ff2127f7
sokiGit <soki.personal@gmail.com>
2023-11-07 13:10:13 +0100

    added sections to index.html

--- index.js
+++
@@ -1,7 +0,0 @@
-const header = $("header");
-const mobile_hamburger = $(".mobile-hamburger");
-
-mobile_hamburger.bind("touchend", (e) => {
-    header.css("visibility", "visible")
-    mobile_hamburger.css("visibility", "hidden")
-})
