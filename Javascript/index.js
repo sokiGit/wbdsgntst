60fc738dde4b8a511acc28204b671395cdcac267
sokiGit <90055900+sokiGit@users.noreply.github.com>
2023-11-07 19:50:05 +0100

    fixed header, added main (not fully done)

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,20 @@
 const header = $("header");
 const mobile_hamburger = $(".mobile-hamburger");
+const too_early = $(".too-early")
+const too_early_close = $(".too-early-close");
 
 mobile_hamburger.bind("touchend", (e) => {
-    header.css("visibility", "visible")
-    mobile_hamburger.css("visibility", "hidden")
+    if (header.css("visibility") == "hidden")
+    {
+        header.css("visibility", "visible")
+    } else {
+        header.css("visibility", "hidden")
+    }
 })
+
+function early_close() {
+    too_early.css("visibility", "hidden");
+}
+
+too_early_close.bind("click", early_close);
+too_early_close.bind("touched", early_close);
