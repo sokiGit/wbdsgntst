85fd031eaafd5f72bdb6133813f123476a6184af
sokiGit <90055900+sokiGit@users.noreply.github.com>
2023-12-03 09:11:58 +0100

    send-msg fade out, to-bottom scroll, bottom height

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,9 +5,11 @@ const header = $("header");
 const mobile_hamburger = $(".mobile-hamburger");
 
 // Early easter egg
-const too_early = $(".too-early")
+const too_early = $(".too-early");
 const too_early_close = $(".too-early-close");
 
+// Banner
+const to_bottom = $(".to-bottom");
 
 /* Header */
 mobile_hamburger.bind("touchend", (e) => {
@@ -28,3 +30,10 @@ too_early_close.bind("click", early_close);
 too_early_close.bind("touched", early_close);
 
 early_close()
+
+/* Banner */
+to_bottom.click((e) => { 
+    e.preventDefault();
+    window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "smooth"});
+    
+});
