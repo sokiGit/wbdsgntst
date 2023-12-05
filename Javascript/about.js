dad697eed60b92ac779553c2cf598c8515d563cb
sokiGit <soki.pesronal@gmail.com>
2023-12-05 10:55:06 +0100

    aboout mobile support, changed looks of articles

--- Javascript/about.js
+++ Javascript/about.js
@@ -1,6 +1,6 @@
-$(".article-section a").each(() => {
-    $(this).css("background-color", "color-mix( in oklab, "+$(this).attr("data-link-color")+", transparent 50%)");
-    $(this).hover(() => {
-        $(this.css("background-color", $(this).attr("data-link-color")));
+$(".article-section a").each(function() {
+    $(this).css("background-color", "color-mix(in oklab, "+$(this).attr("data-link-color")+", transparent 50%)");
+    $(this).hover((e) => {
+        $(this).css("background-color", e.type === "mouseenter" ? $(this).attr("data-link-color") : "color-mix(in oklab, "+$(this).attr("data-link-color")+", transparent 50%)");
     })
 })
