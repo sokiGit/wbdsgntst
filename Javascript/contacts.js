ac2a3f6a7ea2c0ac9b0f0a4bb24f6d6568101d99
sokiGit <soki.personal@gmail.com>
2024-06-04 12:01:59 +0200

    minor QOL changes

--- Javascript/contacts.js
+++ Javascript/contacts.js
@@ -4,9 +4,9 @@ console.log(btns.length);
 
 for (i = 0; i < btns.length; i++) {
     let btn = btns[i];
-    console.log(btn.getElementsByTagName("a")[0].href);
+    console.log(btn.dataset.href);
     btn.style.setProperty("--hover-color", btn.getAttribute("data-color"));
     btn.onclick = () => {
-        window.location.href = btn.getElementsByTagName("a")[0].href;
+        window.open(btn.dataset.href, "_blank")
     }
 }
